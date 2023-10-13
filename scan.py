3052da955d1d889db57ad8b2ccd3bb76bfb66a6a
ksfi <kerian.yousfi@hotmail.fr>
2023-10-13 23:44:39 +0200

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,4 +1,5 @@
 import manuf
+from IPy import IP
 from scapy.all import ARP, Ether, srp
 
 from typing import Dict, Tuple, List
@@ -20,21 +21,29 @@ class Scan:
         vendor = manuf.MacParser()
         ret_mac: List[str] = []
         ret_arp: Dict[str, str] = dict()
-        k: int = 0
-        for device in devices:
+        for k, device in enumerate(devices):
             ret_mac.append(device['mac'])
             ret_arp[device['mac']] = device['ip']
             print(f"{k} -> {device['ip']} \t\t {device['mac']} \t\t {vendor.get_manuf(device['mac'])}")
-            k+=1
         return ret_mac, ret_arp
 
     @staticmethod
-    def arp_cache() -> None:
+    def arp_cache() -> Tuple[List[str], Dict[str, str]]:
         import subprocess
         result: str = subprocess.check_output("arp -a", shell=True, text=True)
         arp_cache: List[str] = result.strip().split('\n')
-        for entry in arp_cache:
-            print(entry)
+
+        print("\n\tIP Address\t\t\tMAC Address\t\tInfo")
+        print("----------------------------------------------------------------------------")
+        vendor = manuf.MacParser()
+        ret_mac: List[str] = []
+        ret_arp: Dict[str, str] = dict()
+        for k, entry in enumerate(arp_cache):
+            spl: str = entry.split(' ')
+            ret_mac.append(spl[3])
+            ret_arp[spl[3]] = spl[1][1:-1]
+            print(f"{k} -> {spl[1][1:-1]} \t\t {spl[3]} \t\t {vendor.get_manuf(spl[3])}")
+        return ret_mac, ret_arp
 
 if __name__ == "__main__":
     Scan.arp_cache()
